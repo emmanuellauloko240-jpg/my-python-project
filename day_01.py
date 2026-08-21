314826651331c11183f288de8a7e653b3a77b5e3
emmauellauloko240-jpg <emmanunellauloko240@gmail.com>
2026-08-21 14:14:03 +0100

    Practice Python data types and operators

--- day_01.py
+++ day_01.py
@@ -1,9 +1,32 @@
+# Python Day 1 - Variables, Data Types & Operators
+
+# Variables
 name = "Emmanuella"
 age = 25
 favorite_food = "Pizza"
 height = 5.6
+is_learning = True
 
-print("My name is",name)
-print("I am",age,"years old")
-print("My favorite food is",favorite_food)
-print("I am",height,"feet tall")
+# Printing values and their types
+print("My name is", name, "and I am", type(name))
+print("I am", age, "years old and I am", type(age))
+print("My favorite food is", favorite_food, "and I am", type(favorite_food))
+print("I am", height, "feet tall and I am", type(height))
+print("I am learning Python and I am", type(is_learning))
+
+# Data type experiments
+print(25 + 5)
+print("25" + "5")
+
+print(10 * 5)
+print("10" * 5)
+
+name = "Emmanuella"
+print(name * 3)
+
+word = "Hi"
+print(word + "!")
+print(word * 3)
+
+print(5 + 5)
+print(5 * 5)
