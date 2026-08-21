3c8f1c28209831a035736a2421dce2ef8d4657d4
emmauellauloko240-jpg <emmanunellauloko240@gmail.com>
2026-08-21 13:42:29 +0100

    Add Python day 1 basics

---
+++ day_01.py
@@ -0,0 +1,9 @@
+name = "Emmanuella"
+age = 25
+favorite_food = "Pizza"
+height = 5.6
+
+print("My name is",name)
+print("I am",age,"years old")
+print("My favorite food is",favorite_food)
+print("I am",height,"feet tall")
