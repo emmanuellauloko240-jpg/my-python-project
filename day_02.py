6bcbca79e3032ad6de1fdcf5936000cb3cd12fa5
emmanuella240-jpg <emmanuellauloko240@gmail.com>
2026-09-03 14:08:37 +0100

    day2 commit

---
+++ day_02.py
@@ -0,0 +1,32 @@
+name = "Emmanuella"
+
+# String methods
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+# Length
+print(len(name))
+
+# Indexing
+print(name[0])
+print(name[1])
+print(name[-1])
+print(name[-2])
+
+# Slicing
+print(name[0:4])
+print(name[2:6])
+print(name[:3])
+print(name[3:])
+
+# Slicing with step
+word = "Python"
+print(word[0:6:2])
+print(word[1:6:2])
+
+# First and last name
+first_name = "emmanuella"
+last_name = "oluwaseun"
+
+print(first_name.title() + " " + last_name.title())
