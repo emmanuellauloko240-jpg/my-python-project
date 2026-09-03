95cd78cd39823798c075decabc0644a3665e8673
emmanuella240-jpg <emmanuellauloko240@gmail.com>
2026-09-03 16:58:15 +0100

    day2 commit

---
+++ day_04.py
@@ -0,0 +1,14 @@
+#LIST
+
+#fruits = ["apple", "banana", "mango", "orange"]
+#fruits.append('pineapple')
+#fruits.remove("orange")
+#print(fruits)
+
+favorite = ["ice-cream", "pizza", "shawarma","suya"]
+favorite.pop(1)
+favorite.pop()
+favorite[2] = "burger"
+favorite.append("exotic")
+favorite.remove[3]
+print(favorite)
