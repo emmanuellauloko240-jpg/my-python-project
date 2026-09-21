b4bdbfd083b8f7030748d9a5058da93750727d79
emmanuellauloko240@gmail.com <emmanuellauloko240@gmail.com>
2026-09-21 10:59:14 +0100

    initial-commit

--- expense_tracker/main.py
+++ expense_tracker/main.py
@@ -1,18 +1,25 @@
 expenses = []
 
 def expenses_tracker():
+
+    total = 0
     while True:
+        
 
         expense = input("Enter expense: ")
         amount = float(input("Enter amount: "))
+        total += amount
         one_expense = {
             expense: amount
         }
-        expense + amount
+        # expense + amount
         expenses.append(one_expense)
         another = input("Do you want another expenses? (yes/no): ")
         if another == "no" or another == "n":
             break
     print(expenses)
+    print(f"total expenses: ₦{total}")
         
 expenses_tracker()
+
+
